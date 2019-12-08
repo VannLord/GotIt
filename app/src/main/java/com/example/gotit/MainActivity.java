39f085de8c28fd20115ca08fe7299f332a95f4d2
phuochoquang97 <56500909+phuochoquang97@users.noreply.github.com>
2019-12-08 23:01:59 +0700

    MenuBar

--- app/src/main/java/com/example/gotit/MainActivity.java
+++ app/src/main/java/com/example/gotit/MainActivity.java
@@ -1,19 +1,34 @@
 package com.example.gotit;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.googlecode.tesseract.android.TessBaseAPI;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
@@ -25,11 +40,15 @@ public class MainActivity extends AppCompatActivity {
     private Button btnCamera;
     private Bitmap bitmap;
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int IMAGE_PICK_GALLERY_CODE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setSubtitle("Click + button to insert image");
+
 
         prepareTessDataCopy();
         mTess = new TessBaseAPI();
@@ -92,15 +111,105 @@ public class MainActivity extends AppCompatActivity {
 
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            bitmap = imageBitmap;
-            imgInput.setImageBitmap(imageBitmap);
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+
+            if (requestCode == REQUEST_IMAGE_CAPTURE) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                bitmap = imageBitmap;
+
+                int w = bitmap.getWidth();
+                int h = bitmap.getHeight();
+                Matrix mtx = new Matrix();
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+                bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+                imgInput.setImageBitmap(imageBitmap);
+            }
+
+            if (requestCode == IMAGE_PICK_GALLERY_CODE) {
+                //got image from gallery
+                CropImage.activity(data.getData()).setGuidelines(CropImageView.Guidelines.ON).start(this); //enable image guidlines
+            }
         }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri(); // get image uri
+
+                //set image to image view
+                imgInput.setImageURI(resultUri);
+
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), resultUri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 
+    //actionbar menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflate item
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //handle actionbar item clicks
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.addImage)
+        {
+            showImageImportDialog();
+        }
+        if(id == R.id.setting)
+        {
+            Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showImageImportDialog()
+    {
+        String[] items = {" Camerea", " Gallery"};
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        //set title
+        dialog.setTitle("Select Image");
+        dialog.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(which == 0)
+                {
+                    //camera option clicked
+                    takePhoto();
+                }
+                if(which == 1)
+                {
+                    //gallery option clicked
+                    pickGallery();
+                }
+            }
+        });
+        dialog.create().show(); // show dialog
+    }
+
+    private void pickGallery()
+    {
+        //intent to pick image from gallery
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        //set intent type to image
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_GALLERY_CODE);
+    }
+
+    //handle p
+
 }
+
 
 
 
